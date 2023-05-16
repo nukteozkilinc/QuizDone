c06d84842d974bb86327e89ad21aec1e210a25b5
nukteozkilinc <nukteozkilinc06@gmail.com>
2023-05-16 22:34:54 +0300

    Recieve datas for category page

--- QuizApp/C.swift
+++ QuizApp/C.swift
@@ -1,4 +1,8 @@
-struct Constants{
+struct C{
     static let registerSegue = "RegisterToCategory"
     static let loginSegue = "LoginToCategory"
+    
+    struct FireStore {
+        static let quizTitle = "quizTitle"
+    }
 }
---
+++ QuizApp/Category.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Category{
+    let quizDesc : String
+    let quizId : String
+    let quizImgUrl : String
+    let quizTitle : String
+    
+}
+
