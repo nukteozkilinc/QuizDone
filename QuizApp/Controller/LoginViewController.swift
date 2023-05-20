154e2b9ba45862f37a122aa977590d46104bc9d2
nukteozkilinc <nukteozkilinc06@gmail.com>
2023-05-20 23:28:20 +0300

    Complete google auth and add pictures for categories

--- QuizApp/Controller/LoginViewController.swift
+++ QuizApp/Controller/LoginViewController.swift
@@ -5,7 +5,7 @@ import FirebaseCore
 
 
 class LoginViewController : UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -25,29 +25,37 @@ class LoginViewController : UIViewController {
     }
     
     
-    @IBAction func googleAuthPressed(_ sender: Any) {
+    @IBAction func googleAuthPressed(_ sender: UIButton) {
+        
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
-
+        
         // Create Google Sign In configuration object.
         let config = GIDConfiguration(clientID: clientID)
         GIDSignIn.sharedInstance.configuration = config
-
+        
         // Start the sign in flow!
         GIDSignIn.sharedInstance.signIn(withPresenting: self) { result, error in
             if let e = error{
                 print(e)
             }
-
-          guard let user = result?.user,
-            let idToken = user.idToken?.tokenString
-          else {
-            return self.performSegue(withIdentifier: C.loginSegue, sender: self) // HATA VAR!!!
-          }
-
-          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
-                                                         accessToken: user.accessToken.tokenString)
-
-          // ...
+            
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString
+            else {
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    print("Login Successful.")
+                    self.performSegue(withIdentifier: C.loginSegue, sender: self)
+                }
+            }
         }
     }
 }
