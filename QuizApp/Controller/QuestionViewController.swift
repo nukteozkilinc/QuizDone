1b968f82474c81cb56215622ed0e53a22b022569
nukteozkilinc <nukteozkilinc06@gmail.com>
2023-05-24 16:30:05 +0300

    Receive datas from realtime database

--- QuizApp/Controller/QuestionViewController.swift
+++ QuizApp/Controller/QuestionViewController.swift
@@ -11,43 +11,28 @@ class QuestionViewController : UIViewController{
     let db = Firestore.firestore()
     var categoryIndex : String?
     var ref: DatabaseReference!
+    var questionsArray : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.ref = Database.database(url: "https://quizdone-e2e34-default-rtdb.firebaseio.com/").reference()
         
         questionLabel.text = categoryIndex
-        getData()        
+        getData()
     }
     
     func getData(){
-        /*ref.child("questions").child(categoryIndex ?? "g5").observeSingleEvent(of: .value, with: { snapshot in
-          // Get user value
-          let value = snapshot.value as? NSDictionary
-          let subject = value?["subject"] as? String ?? ""
-          let title = value?["title"] as? String ?? ""
-          let options = value?["options"] as? [String : Bool]
-          let question = Question(title: title, subject: subject, options: options!)
-            print(self.categoryIndex ?? "Gelmedi")
-            print(question.subject)
-            print(question.title)
-            print(question.options)
-          // ...
-        }) { error in
-          print(error.localizedDescription)
-        }*/
+        questionsArray = []
         ref.child("questions").observeSingleEvent(of: .value, with: { snapshot in
-          // Get user value
-          let value = snapshot.value as? NSDictionary
-          let subject = value?["subject"] as? String ?? ""
-          let title = value?["title"] as? String ?? ""
-          let options = value?["options"] as? [String : Bool]
-          let question = Question(title: title, subject: subject, options: options!)
-            print(self.categoryIndex ?? "Gelmedi")
-            print(question.subject)
-            print(question.title)
-            print(question.options)
-          // ...
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let placeDict = snap.value as! [String: Any]
+                let subject = placeDict["subject"] as! String
+                let title = placeDict["title"] as! String
+                if subject == self.categoryIndex{
+                    self.questionsArray.append(title)
+                }
+            }
         }) { error in
           print(error.localizedDescription)
         }
